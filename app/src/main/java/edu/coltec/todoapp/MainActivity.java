9745f4c22bbd8c7f4de61272e7a74341e6e3f2a7
coltec <coltec@coltec.com>
2022-06-22 19:16:29 -0300

    Estado inicial do projeto

---
+++ app/src/main/java/edu/coltec/todoapp/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.coltec.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.coltec.todoapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
